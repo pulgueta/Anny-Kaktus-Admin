5bac99e000fc7e261ffdabd6eca791d49f69d950
pulgueta <retardix456@gmail.com>
2022-05-02 14:56:03 -0500

    Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,29 @@
 module.exports = {
-  content: [
-	"./index.html",
-	"./src/**/*.{vue,js,ts,tsx,jsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        flora: {
+          base: "#4baf56",
+          second: "#e73f3f",
+          hover: "#4bffaa",
+          secondhover: "#ff3f3f",
+          black: "#393939",
+          white: "#f1f7f2",
+        },
+      },
+
+      textColor: {
+        flora: {
+          base: "#4baf56",
+          second: "#e73f3f",
+          hover: "#4bffaa",
+          secondhover: "#e73f3f",
+          black: "#393939",
+          white: "#f1f7f2",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
